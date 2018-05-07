e81ef44a9c2dbabb26ef8c397bce96c0938287cd
zhg029 <zhg029@ucsd.edu>
2018-05-06 19:27:29 -0700

    Created new features.

--- clustering.py
+++ clustering.py
@@ -6,8 +6,16 @@ import random
 import numpy as np
 from collections import Counter
 from datetime import datetime
+from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.cluster import KMeans
+
+plt.interactive(False)
+plt.show(block=True)
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+print('x' in np.arange(5))
 
 USERINFO_PATH = 'userinfo_labeled.json'
 
@@ -20,9 +28,10 @@ profile_pics = [account['profile_pic'] for account in userinfo if 'profile_pic' 
 # for account in userinfo:
 #     if 'profile_pic' in account.keys():
 #         profile_pics.append(account['profile_pic'])
-print(profile_pics[:5])         # see first 5 entries
+# see first 5 entries
 profile_pic_counts = Counter(profile_pics)
 high_counts = [url for url, count in profile_pic_counts.items() if count > 1]
+print(high_counts)
     
 # no followers, no followed: follow:followed ratio
 # follow few accounts but repost everyday
@@ -32,47 +41,43 @@ high_counts = [url for url, count in profile_pic_counts.items() if count > 1]
 # activity trend: bursts of activity and then inactivity etc
 
 def make_features(account):
-    """Take userinfo and convert to useable features"""
-    num_badges = len(account['badges'])
-    if 'club' in account.keys():
-        club_status = True
-        if account['club'] == False:
-            club_status = False
-        elif account['club'] == '微博达人' or account['club'] == 'True': 
-            club_status = True
-    elif 'club_level' in account.keys():
-        club_status = True
+    if 'profile_pic' in account.keys():
+        profile_pic = True
+        if account ['profile_pic'] == '//ww1.sinaimg.cn/default/images/default_avatar_male_uploading_180.gif':
+            profile_pic = False
+        elif account['profile_pic'] == '//ww1.sinaimg.cn/default/images/default_avatar_female_uploading_180.gif':
+            profile_pic = False
+        else:
+            profile_pic = True
     else:
-        club_status = False
-    if 'credit_num' in account.keys():
-        credit = account['credit_num']
-    else:
-        credit = 0
-    level = account['level']
-    if 'vip' in account.keys() or 'vip_speed' in account.keys():
-        vip = True
-    else:
-        vip = False
+        profile_pic = False
     reg_date = datetime.strptime(account['注册时间'], '%Y-%m-%d')
-    account_age = datetime.now() - reg_date
-    account_age = account_age.days
-    if '关注' in account.keys():
-        following_followers = account['关注']/account['粉丝']
-    elif account['粉丝'] == 0:
-        following_followers = np.inf
-    else:
-        following_followers = 0
     elapsed_time = datetime.strptime(account['checkTime'], '%Y%m%d-%H%M%S') - reg_date
+    if '关注' in account.keys():
+        followers = True
+        if account['关注'] == 0:
+            followers = False
+        else:
+                followers = True
+    else:
+        followers = False
+    if '粉丝' in account.keys():
+        followed = True
+        if account['粉丝'] == 0:
+            followed = False
+        else:
+            followed = True
+    else:
+        followed = False
     if '微博' in account.keys():
-        posts_perday = account['微博']/elapsed_time.days
+        posts_perday = account['微博'] / elapsed_time.days
     else:
         posts_perday = 0
-    if '标签' in account.keys():
-        num_tabs = len(account['标签'])
+    if '关注' in account.keys():
+        repost_ratio = account['关注']/posts_perday
     else:
-        num_tabs = 0
-    return [num_badges, club_status, credit, level, vip, account_age, following_followers,
-            posts_perday, num_tabs]
+        repost_ratio = 0
+    return [profile_pic, followers, followed, repost_ratio]
 
 feature_list = []
 userids = []
@@ -85,7 +90,8 @@ for account in userinfo:
         print(account)
 feature_array = np.array(feature_list)
 
-# using hierarchical clustering
+print(feature_list)
+
 def display_dendrogram(idx='all'):
     """Run hierarchical clustering with Ward linkage and show clustogram"""
     if idx == 'all':
@@ -97,17 +103,6 @@ def display_dendrogram(idx='all'):
     hier_clust = linkage(to_cluster, method='ward')
     dendrogram(hier_clust, labels=the_labels, orientation='left')
 
-# very difficult to see anything, look at random userids
 idx = np.random.randint(feature_array.shape[0], size=25)
-display_dendrogram(idx)         # look for patterns
-
-# using kmeans
-kmeans_n_clusters = 8           # experiment with different k
-kmeans = KMeans(n_clusters=kmeans_n_clusters)
-kmeans.fit(feature_array)
-kmeans_assignment = kmeans.predict(feature_array)
-
-kmeans_cluster_assignment = [[] for x in range(kmeans_n_clusters)]
-for i, cluster in enumerate(kmeans_assignment):
-    kmeans_cluster_assignment[cluster].append(userids[i])
-# look through the resulting object for patterns
+display_dendrogram(idx)
+plt.show()
